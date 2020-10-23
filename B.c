9561af774ea72b391480747544b53ea784044236
NeerajSahani <Neerajsahani9@gmail.com>
2020-10-23 18:35:13 +0530

    Initial

---
+++ B.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int i, j, n;
+	printf("Enter Size :");
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			if (i == 1 || j == 1 || i == n || j == n || i == (n / 2) + 1 )
+				printf("#");
+			else
+				printf(" ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
---
+++ F.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	int i, j, n;
+	printf("Enter Size :");
+	scanf("%d", &n);
+	n = 10;
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			if (i == 1 || j == 1 || i == n/2)
+				printf("#");
+			else
+				printf(" ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
